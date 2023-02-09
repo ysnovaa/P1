3e690007009588c7f1df3ca8b5bd3c0eb524080b
ysnovaa <yobancopia11@gmail.com>
2023-02-09 11:24:00 -0500

    App movie V1

---
+++ MOVIEPROYECT/moviereviews/movie/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def home(request):
+    #return HttpResponse('<h1>Bienvenido a mi pagina de inicio papu</h1>')
+    #return render(request, 'home.html')
+    return render(request, 'home.html', {'name':'Yoban Nova'})
