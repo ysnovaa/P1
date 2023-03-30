b55ad6d14cbd166a472181995fd614f00f7e4fae
ysnovaa <yobancopia11@gmail.com>
2023-03-30 11:33:52 -0500

    Taller 3

---
+++ MOVIEPROYECT/moviereviews/movie/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+from .models import Movie
+
+def home(request):
+    searchTerm = request.GET.get('searchMovie')
+    if searchTerm:
+        movies = Movie.objects.filter(title__icontains=searchTerm)
+    else:
+        movies = Movie.objects.all()
+    return render(request, 'home.html', {'searchTerm':searchTerm,'movies': movies})
+    
+def about(request):
+   return render(request, 'about.html', {'name':'Yoban Nova '})
